b8502e487731c82c53d07658807dd9b3ef8a1263
CharisLai <griffin8418@gmail.com>
2023-08-09 11:58:10 +0800

    feat: MenuOverlay

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -118,7 +118,7 @@
                             </div>
                     </button>
                 </NuxtLink>
-                <button @click="useStore.isMenuOverlay = true" 
+                <button @click="userStore.isMenuOverlay = true" 
                         class="md:hidden block rounded-full p-1.5 -mt-[4px] hover:bg-gray-200">
                     <Icon name="radix-icons:hamburger-menu" size="33" />
                 </button>
