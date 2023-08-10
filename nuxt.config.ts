4447c01e8c6624d46e1e5397dac4274ee98a3baf
CharisLai <griffin8418@gmail.com>
2023-08-10 19:12:10 +0800

    feat: add orders page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,9 @@ export default defineNuxtConfig({
       '@nuxtjs/tailwindcss',
       '@nuxtjs/supabase'
   ],
+  // supabase: {
+  //   redirect: false,
+  // },
   runtimeConfig: {
       public: {
         stripePk: process.env.STRIPE_PK_KEY
